05a04d1a6beac24f3c0580b47531e6a087af3dde
unknown <awalupshot@gmail.com>
2022-08-23 20:20:30 +0545

    new commit

--- backend/controllers/apparelController.js
+++ backend/controllers/apparelController.js
@@ -1,11 +1,86 @@
+const e = require("express");
 const asyncHandler = require("express-async-handler");
+const Apparel = require("../models/apparelModel")
 
-const addApparel = asyncHandler(async (req, res) => {});
-const editApparel = asyncHandler(async (req, res) => {});
-const updateApparel = asyncHandler(async (req, res) => {});
+const addApparel = asyncHandler(async(req, res) => {
+    const { name, price, size, description, category, relatedTo } = req.body;
+
+    const prodExist = await Apparel.findOne({ name });
+    if (prodExist) {
+        res.status(400);
+        throw new Error("Product Name Already exists")
+    }
+
+    //adds new product to database
+    const product = await Apparel.create({
+        name,
+        price,
+        description,
+        category,
+        relatedTo,
+        size,
+    });
+    if (product) {
+        res.status(201).json({
+            _id: product.id,
+            name: product.name,
+            price: product.price,
+            description: product.description,
+            category: product.category,
+            relatedTo: product.relatedTo,
+            size: product.size
+        });
+    } else {
+        res.status(500);
+        throw new Error(`Server Error`)
+    }
+});
+
+// delete the product prom database
+const deleteApparel = asyncHandler(async(req, res) => {
+    const { _id } = req.body;
+    const product = await Apparel.findOne({ _id });
+    if (!product) {
+        res.status(400);
+        throw new Error('Product Dosent Exists')
+    }
+    const deleteProduct = await product.remove()
+    if (!deleteProduct) {
+        return res
+            .status(500)
+            .json({ message: `Failed to delete product with id ${_id}` });
+    }
+    res.status(200).json({
+        sucess: true,
+        message: "product delete sucessfully"
+    });
+
+});
+
+
+//update a certain product from database
+const updateApparel = asyncHandler(async(req, res) => {
+    const _id = req.params.id;
+
+    if (Object.keys(req.body).length === 0) {
+        res.status(400);
+        throw new Error('No Body to Update')
+    }
+
+    const updateProduct = await Apparel.findByIdAndUpdate(_id, req.body, { new: false, });
+    if (!updateProduct) {
+        return res
+            .status(500)
+            .json({ message: `Failed to update product with id ${_id}` });
+    }
+    res.status(200).json({
+        sucess: true,
+        message: "Product updated sucessfully"
+    })
+});
 
 module.exports = {
-  addApparel,
-  editApparel,
-  updateApparel,
+    addApparel,
+    deleteApparel,
+    updateApparel,
 };
--- backend/routes/apparelRoute.js
+++ backend/routes/apparelRoute.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const { findOneAndReplace } = require("../models/apparelModel");
 const {
-  addApparel,
-  editApparel,
-  updateApparel,
+    addApparel,
+    deleteApparel,
+    updateApparel,
 } = require("../controllers/apparelController");
+const { model } = require("mongoose");
 
 router.post("/", addApparel);
+router.delete("/", deleteApparel);
+router.patch("/:id", updateApparel);
+
+module.exports = router;
